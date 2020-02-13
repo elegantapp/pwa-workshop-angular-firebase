9818d641606d54ad50f40d57681179baf868a043
Önder Ceylan <onderceylan@gmail.com>
2020-02-13 22:38:13 +0100

    Updated Step-15 solution

--- server/sendPush.js
+++ server/sendPush.js
@@ -1,42 +1,44 @@
 const webpush = require('web-push');
 
-webpush.setGCMAPIKey(process.env.FIREBASE_SERVER_API_KEY);
-webpush.setVapidDetails(
-  `mailto:${process.env.EMAIL_FOR_SUBJECT}`,
-  process.env.FIREBASE_WEB_PUSH_PUBLIC_VAPID_KEY,
-  process.env.FIREBASE_WEB_PUSH_PRIVATE_VAPID_KEY
-);
+(async () => {
 
-// This is the output of calling JSON.stringify on a PushSubscription you receive on your client
-// Copy paste the console log of push subscription from the receiver client here
-const pushSubscription = {
-  endpoint: '...',
-  keys: {
-    auth: '...',
-    p256dh: '...'
-  }
-};
+  webpush.setGCMAPIKey(process.env.FIREBASE_SERVER_API_KEY);
+  webpush.setVapidDetails(
+    `mailto:${process.env.EMAIL_FOR_SUBJECT}`,
+    process.env.FIREBASE_WEB_PUSH_PUBLIC_VAPID_KEY,
+    process.env.FIREBASE_WEB_PUSH_PRIVATE_VAPID_KEY
+  );
 
-const notificationPayload = {
-  notification: {
-    title: 'Session is about the start 🏃‍♀️',
-    body: '"Community Interaction" by Gino Giraffe is starting in Hall 3.',
-    icon: 'assets/pwa/manifest-icon-192.png',
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-    },
-    actions: [
-      {
-        action: 'session:10',
-        title: 'Session info 👉',
+  // This is the output of calling JSON.stringify on a PushSubscription you receive on your client
+  // Copy paste the console log of push subscription from the receiver client here
+  const pushSubscription = {
+    endpoint: '...',
+    keys: {
+      auth: '...',
+      p256dh: '...'
+    }
+  };
+
+  const notificationPayload = {
+    notification: {
+      title: 'Session is about the start 🏃‍♀️',
+      body: '"Community Interaction" by Gino Giraffe is starting in Hall 3.',
+      icon: 'assets/pwa/manifest-icon-192.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
       },
-      {
-        action: 'speaker:6',
-        title: 'Speaker info 🗣',
-      }
-    ]
-  }
-};
+      actions: []
+    }
+  };
 
-webpush.sendNotification(pushSubscription, JSON.stringify(notificationPayload));
+  try {
+    // Send the push notification
+    await webpush.sendNotification(pushSubscription, JSON.stringify(notificationPayload));
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+
+})();
